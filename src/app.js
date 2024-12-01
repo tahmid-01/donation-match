3e30aa37b0ce7425d201596710ab9e0548611547
mosefatullah <mosefatullah@gmail.com>
2024-12-01 17:32:16 +0600

    setup #1

--- src/app.js
+++ src/app.js
@@ -1,50 +1,44 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const path = require("path");
 const cors = require("cors");
 
-const UserController = require("./controllers/UserController");
-const InventoryController = require("./controllers/InventoryController");
+const connectDB = require("./config/mongodb");
 const PORT = process.env.PORT || 3000;
 
-/* Load environment variables */
+/*** load environment variables */
 if (process.env.NODE_ENV === "development") {
- dotenv.config({ path: path.resolve(__dirname, ".env.local") });
+ dotenv.config({ path: path.resolve(__dirname, "../.env.local") });
 } else {
  dotenv.config();
 }
 
-/* express app initialization */
+/*** express app initialization */
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "frontend/dist")));
+// app.use(express.static(path.join(__dirname, "frontend/dist")));
 app.use(
  cors({
   origin: "*",
  })
 );
 
-/* database connection */
-mongoose
- .connect(process.env.MONGODB_URI)
- .then(() => console.log("Connected to database"))
- .catch((err) => console.log(err));
+/*** database connection */
+connectDB();
 
-/* application routes */
+/*** application routes */
 app.get("/api/v1", (req, res) => {
  res.json({
-  name: "The Blood Project API",
+  name: "Donation Match API",
   version: "1.0.0",
  });
 });
-app.use("/api/v1/user", UserController);
-app.use("/api/v1/inventory", InventoryController);
+//app.use("/api/v1/users", userRoutes -> UserController);
 app.get("*", (req, res) => {
- res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+ // res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
 });
 
-/* default error handler */
+/*** default error handler */
 app.use((err, req, res, next) => {
  if (req.headersSent) {
   return next(err);
@@ -54,6 +48,7 @@ app.use((err, req, res, next) => {
  });
 });
 
+/*** server setup */
 app.listen(PORT, () => {
  console.log(
   `Running on http://localhost:${PORT} - ${
--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
  try {
-  await mongoose.connect(process.env.MONGO_URI, {
-   useNewUrlParser: true,
-   useUnifiedTopology: true,
-  });
-  console.log("MongoDB connected");
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log("* MongoDB connected");
  } catch (error) {
-  console.error("MongoDB connection failed:", error.message);
+  console.error("* MongoDB connection failed:", error.message);
   process.exit(1);
  }
 };
